3f25ef790db8c907bdb25bb9facd5ab04400aee2
Theodore Kelechukwu Onyejiaku <theodoreonyejiaku@gmail.com>
2024-07-07 14:19:23 +0100

    change adapter from node to vercel

--- ui/astro.config.mjs
+++ ui/astro.config.mjs
@@ -1,15 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import node from "@astrojs/node";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-    i18n: {
-        defaultLocale: 'en',
-        locales: ['es', 'en']
-    },
-    output: "server",
-    adapter: node({
-        mode: "standalone"
-    })
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['es', 'en']
+  },
+  output: "server",
+  adapter: vercel()
 });
